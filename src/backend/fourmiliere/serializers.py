4939cc0cd29e2233fda54f854b16d0d2d52db6ef
baitieyi <baitieyi@163.com>
2019-07-20 21:04:32 +0800

    序列化 Post

--- src/backend/fourmiliere/serializers.py
+++ src/backend/fourmiliere/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Post
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'password')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'username', 'content', 'create_time', 'like', 'hate')
