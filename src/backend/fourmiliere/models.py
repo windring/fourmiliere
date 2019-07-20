d4d660c4e4fb46825c08b71315aba93c30db17ab
baitieyi <baitieyi@163.com>
2019-07-20 21:02:15 +0800

    添加 Post 的 model

--- src/backend/fourmiliere/models.py
+++ src/backend/fourmiliere/models.py
@@ -5,3 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     class Meta(AbstractUser.Meta):
         pass
+
+
+class Post(models.Model):
+    username = models.ForeignKey('User', on_delete=models.SET_NULL)
+    content = models.CharField(max_length=2000)
+    create_time = models.BigIntegerField()
+    like = models.IntegerField()
+    hate = models.IntegerField()
